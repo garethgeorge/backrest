b3f062a39a7a3cf56f90a877e714d25c0473d285
garethgeorge <garethgeorge97@gmail.com>
2023-11-11 13:40:37 -0800

    feat: scaffolding basic UI structure

--- webui/src/views/App.tsx
+++ webui/src/views/App.tsx
@@ -1,24 +1,79 @@
-import test from "node:test";
-import * as React from "react";
-import _ from "lodash";
+import React from "react";
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
 
-export const App = () => {
-  const [searchTerm, setSearchTerm] = React.useState("");
-  const [results, setResults] = React.useState<any>();
+const { Header, Content, Sider } = Layout;
+
+const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2: MenuProps["items"] = [
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+].map((icon, index) => {
+  const key = String(index + 1);
+
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+
+export const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, colorTextLightSolid },
+  } = theme.useToken();
 
   return (
-    <div>
-      <h1>Guidebook FS Search</h1>
-      <div>
-        Search:
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        ></input>
-      </div>
-      <pre>
-        <code>{"RESULTS: " + JSON.stringify(results, null, 2)}</code>
-      </pre>
-    </div>
+    <Layout>
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <h1 style={{ color: colorTextLightSolid }}>ResticUI</h1>
+      </Header>
+      <Layout>
+        <Sider width={200} style={{ background: colorBgContainer }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%", borderRight: 0 }}
+            items={items2}
+          />
+        </Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 };
