16fbee81405438be17106af0ea39108b8a01960f
garethgeorge <garethgeorge97@gmail.com>
2023-11-30 23:35:04 -0800

    chore: scheduledtaskheap is ordered by time and priority

--- internal/orchestrator/scheduledtaskheap.go
+++ internal/orchestrator/scheduledtaskheap.go
@@ -10,8 +10,9 @@ import (
 type taskQueue struct {
 	dequeueMu sync.Mutex
 	mu        sync.Mutex
-	heap      scheduledTaskHeap
+	heap      scheduledTaskHeapByTime
 	notify    chan struct{}
+	ready     scheduledTaskHeapByPriorityThenTime
 
 	Now func() time.Time
 }
@@ -42,7 +43,10 @@ func (t *taskQueue) Reset() []*scheduledTask {
 	defer t.mu.Unlock()
 
 	oldTasks := t.heap.tasks
+	oldTasks = append(oldTasks, t.ready.tasks...)
 	t.heap.tasks = nil
+	t.ready.tasks = nil
+
 	if t.notify != nil {
 		t.notify <- struct{}{}
 	}
@@ -62,6 +66,10 @@ func (t *taskQueue) Dequeue(ctx context.Context) *scheduledTask {
 	for {
 		first, ok := t.heap.Peek().(*scheduledTask)
 		if !ok { // no tasks in heap.
+			if t.ready.Len() > 0 {
+				t.mu.Unlock()
+				return heap.Pop(&t.ready).(*scheduledTask)
+			}
 			t.mu.Unlock()
 			select {
 			case <-ctx.Done():
@@ -71,22 +79,45 @@ func (t *taskQueue) Dequeue(ctx context.Context) *scheduledTask {
 			t.mu.Lock()
 			continue
 		}
+
+		now := t.curTime()
+
+		// if there's a task in the ready queue AND the first task isn't ready yet then immediately return the ready task.
+		ready, ok := t.ready.Peek().(*scheduledTask)
+		if ok && now.Before(first.runAt) {
+			heap.Pop(&t.ready)
+			t.mu.Unlock()
+			return ready
+		}
+
 		t.mu.Unlock()
-		timer := time.NewTimer(first.runAt.Sub(t.curTime()))
+		timer := time.NewTimer(first.runAt.Sub(now))
+
 		select {
 		case <-timer.C:
 			t.mu.Lock()
 			if t.heap.Len() == 0 {
 				break
 			}
-			first = t.heap.Peek().(*scheduledTask)
-			if first.runAt.After(t.curTime()) {
-				// task is not yet ready to run
+
+			for {
+				first, ok := t.heap.Peek().(*scheduledTask)
+				if !ok {
+					break
+				}
+				if first.runAt.After(t.curTime()) {
+					// task is not yet ready to run
+					break
+				}
+				heap.Pop(&t.heap) // remove the task from the heap
+				heap.Push(&t.ready, first)
+			}
+
+			if t.ready.Len() == 0 {
 				break
 			}
-			heap.Pop(&t.heap) // remove the task from the heap
 			t.mu.Unlock()
-			return first
+			return heap.Pop(&t.ready).(*scheduledTask)
 		case <-t.notify: // new task was added, loop again to ensure we have the earliest task.
 			t.mu.Lock()
 			if !timer.Stop() {
@@ -102,22 +133,18 @@ func (t *taskQueue) Dequeue(ctx context.Context) *scheduledTask {
 }
 
 type scheduledTask struct {
-	task  Task
-	runAt time.Time
+	task     Task
+	runAt    time.Time
+	priority int
 }
 
 type scheduledTaskHeap struct {
-	tasks []*scheduledTask
+	tasks      []*scheduledTask
+	comparator func(i, j *scheduledTask) bool
 }
-
-var _ heap.Interface = &scheduledTaskHeap{}
 
 func (h *scheduledTaskHeap) Len() int {
 	return len(h.tasks)
-}
-
-func (h *scheduledTaskHeap) Less(i, j int) bool {
-	return h.tasks[i].runAt.Before(h.tasks[j].runAt)
 }
 
 func (h *scheduledTaskHeap) Swap(i, j int) {
@@ -141,4 +168,27 @@ func (h *scheduledTaskHeap) Peek() interface{} {
 		return nil
 	}
 	return h.tasks[0]
+}
+
+type scheduledTaskHeapByTime struct {
+	scheduledTaskHeap
+}
+
+var _ heap.Interface = &scheduledTaskHeapByTime{}
+
+func (h *scheduledTaskHeapByTime) Less(i, j int) bool {
+	return h.tasks[i].runAt.Before(h.tasks[j].runAt)
+}
+
+type scheduledTaskHeapByPriorityThenTime struct {
+	scheduledTaskHeap
+}
+
+var _ heap.Interface = &scheduledTaskHeapByPriorityThenTime{}
+
+func (h *scheduledTaskHeapByPriorityThenTime) Less(i, j int) bool {
+	if h.tasks[i].priority != h.tasks[j].priority {
+		return h.tasks[i].priority > h.tasks[j].priority
+	}
+	return h.tasks[i].runAt.Before(h.tasks[j].runAt)
 }
