fc19a6cea89e2c95e8afd87ee9ae3e7684b25dba
Gareth George <garethgeorge97@gmail.com>
2023-12-26 08:26:01 +0000

    fix: reduce stats refresh frequency

--- internal/orchestrator/taskstats.go
+++ internal/orchestrator/taskstats.go
@@ -7,8 +7,11 @@ import (
 	"time"
 
 	v1 "github.com/garethgeorge/backrest/gen/go/v1"
+	"github.com/garethgeorge/backrest/internal/oplog/indexutil"
 	"go.uber.org/zap"
 )
+
+var statBytesThreshold int64 = 1024 * 1024 * 1024 // 1GB
 
 // StatsTask tracks a restic stats operation.
 type StatsTask struct {
@@ -18,7 +21,7 @@ type StatsTask struct {
 	at           *time.Time
 }
 
-var _ Task = &ForgetTask{}
+var _ Task = &StatsTask{}
 
 func NewOneoffStatsTask(orchestrator *Orchestrator, plan *v1.Plan, linkSnapshot string, at time.Time) *StatsTask {
 	return &StatsTask{
@@ -35,7 +38,39 @@ func (t *StatsTask) Name() string {
 	return fmt.Sprintf("stats for plan %q", t.plan.Id)
 }
 
+func (t *StatsTask) shouldRun() (bool, error) {
+	var bytesSinceLastStat int64
+	if err := t.orch.OpLog.ForEachByRepo(t.plan.Repo, indexutil.CollectLastN(50), func(op *v1.Operation) error {
+		if _, ok := op.Op.(*v1.Operation_OperationStats); ok {
+			bytesSinceLastStat = 0
+		} else if backup, ok := op.Op.(*v1.Operation_OperationBackup); ok && backup.OperationBackup.LastStatus != nil {
+			if summary, ok := backup.OperationBackup.LastStatus.Entry.(*v1.BackupProgressEntry_Summary); ok {
+				bytesSinceLastStat += summary.Summary.DataAdded
+			}
+		}
+		return nil
+	}); err != nil {
+		return false, fmt.Errorf("iterate oplog: %w", err)
+	}
+
+	zap.L().Debug("bytes since last stat", zap.Int64("bytes", bytesSinceLastStat), zap.String("repo", t.plan.Repo))
+
+	if bytesSinceLastStat > statBytesThreshold {
+		zap.S().Debugf("bytes since last stat (%v) exceeds threshold (%v)", bytesSinceLastStat, statBytesThreshold)
+		return true, nil
+	}
+	return false, nil
+}
+
 func (t *StatsTask) Next(now time.Time) *time.Time {
+	shouldRun, err := t.shouldRun()
+	if err != nil {
+		zap.S().Errorf("task %v failed to check if it should run: %v", t.Name(), err)
+	}
+	if !shouldRun {
+		return nil
+	}
+
 	ret := t.at
 	if ret != nil {
 		t.at = nil
