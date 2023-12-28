07d43887c59abe04711020abf3652d8154709241
Gareth George <garethgeorge97@gmail.com>
2023-12-28 08:18:36 +0000

    feat: add mobile layout

---
+++ webui/src/lib/browserutil.ts
@@ -0,0 +1,8 @@
+export const isMobile = () => {
+  // check if window is narrow 
+  if (window.innerWidth <= 768) {
+    return true;
+  }
+  // check if user agent is mobile
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
