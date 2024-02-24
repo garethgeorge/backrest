56186ba51906d111a0147f5b3c18e18aa26e0a35
Janis Jansons <janhouse@gmail.com>
2024-02-24 13:38:53 +0000

    feat: check for basic auth (#110)

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -17,6 +17,17 @@ const UserContextKey contextKey = "user"
 
 func RequireAuthentication(h http.Handler, auth *Authenticator) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		username, password, usesBasicAuth := r.BasicAuth()
+		if usesBasicAuth {
+			user, err := auth.Login(username, password)
+			if err == nil {
+				ctx := context.WithValue(r.Context(), UserContextKey, user)
+				h.ServeHTTP(w, r.WithContext(ctx))
+				return
+			}
+		}
+
 		token, err := ParseBearerToken(r.Header.Get("Authorization"))
 		if err != nil {
 			http.Error(w, "Unauthorized (No Authorization Header)", http.StatusUnauthorized)
