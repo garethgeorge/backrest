3930a2553d851111a398a8932e59732267e8c433
garethgeorge <garethgeorge97@gmail.com>
2024-04-11 01:46:50 -0700

    fix: bugs in refactored task queue and improved coverage

--- internal/queue/timepriorityqueue_test.go
+++ internal/queue/timepriorityqueue_test.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"context"
+	"math/rand"
 	"testing"
 	"time"
 )
@@ -51,5 +52,35 @@ func TestTPQMixedReadinessStates(t *testing.T) {
 				t.Errorf("pass %d expected %d, got %d", j, i, v)
 			}
 		}
+	}
+}
+
+func TestTPQStress(t *testing.T) {
+	tpq := NewTimePriorityQueue[int]()
+	start := time.Now()
+
+	totalEnqueued := 0
+	totalEnqueuedSum := 0
+
+	go func() {
+		ctx, _ := context.WithDeadline(context.Background(), start.Add(1*time.Second))
+		for ctx.Err() == nil {
+			v := rand.Intn(100)
+			tpq.Enqueue(time.Now().Add(time.Duration(rand.Intn(1000)-500)*time.Millisecond), rand.Intn(5), v)
+			totalEnqueuedSum += v
+			totalEnqueued++
+		}
+	}()
+
+	ctx, _ := context.WithDeadline(context.Background(), start.Add(3*time.Second))
+	totalDequeued := 0
+	sum := 0
+	for ctx.Err() == nil || totalDequeued < totalEnqueued {
+		sum += tpq.Dequeue(ctx)
+		totalDequeued++
+	}
+
+	if sum != totalEnqueuedSum {
+		t.Errorf("expected sum to be %d, got %d", totalEnqueuedSum, sum)
 	}
 }
