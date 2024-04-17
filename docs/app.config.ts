e32e11ee8decf0fc619a6c94e148218e95f413dd
garethgeorge <garethgeorge97@gmail.com>
2024-04-17 00:39:54 -0700

    chore: initial documentation site setup

---
+++ docs/app.config.ts
@@ -0,0 +1,39 @@
+// https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
+export default defineAppConfig({
+  docus: {
+    title: 'Docus',
+    description: 'The best place to start your documentation.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'nuxt_js',
+      github: 'nuxt-themes/docus',
+      nuxt: {
+        label: 'Nuxt',
+        icon: 'simple-icons:nuxtdotjs',
+        href: 'https://nuxt.com'
+      }
+    },
+    github: {
+      dir: '.starters/default/content',
+      branch: 'main',
+      repo: 'docus',
+      owner: 'nuxt-themes',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
