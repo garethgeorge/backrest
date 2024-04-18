81dee1871c1e981ef9d83f7bd1df1bed7d3b8df3
garethgeorge <garethgeorge97@gmail.com>
2024-04-18 00:11:41 -0700

    chore: fix github actions workflow for docs site

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,13 +1,8 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
-
-  modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
-  ],
-
   ssr: true,
+  app: {
+    baseURL: '/backrest/',
+  }
 })
