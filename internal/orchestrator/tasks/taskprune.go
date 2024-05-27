c58055ec91ccc9a8afc5d3ff402f68da00a04e66
garethgeorge <garethgeorge97@gmail.com>
2024-05-27 14:06:36 -0700

    fix: improve prune and check scheduling in new repos

--- internal/orchestrator/tasks/taskprune.go
+++ internal/orchestrator/tasks/taskprune.go
@@ -71,7 +71,7 @@ func (t *PruneTask) Next(now time.Time, runner TaskRunner) (ScheduledTask, error
 	}); err != nil {
 		return NeverScheduledTask, fmt.Errorf("finding last prune run time: %w", err)
 	} else if !foundBackup {
-		return NeverScheduledTask, nil
+		lastRan = time.Now()
 	}
 
 	zap.L().Debug("last prune time", zap.Time("time", lastRan), zap.String("repo", t.RepoID()))
