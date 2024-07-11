d9e6dde17310c934997d42af808afa4f14057d03
garethgeorge <garethgeorge97@gmail.com>
2024-07-11 01:00:26 -0700

    set an appropriate default shell for windows

--- internal/hook/types/command.go
+++ internal/hook/types/command.go
@@ -2,9 +2,11 @@ package types
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os/exec"
 	"reflect"
+	"runtime"
 	"strings"
 
 	v1 "github.com/garethgeorge/backrest/gen/go/v1"
@@ -12,6 +14,7 @@ import (
 	"github.com/garethgeorge/backrest/internal/ioutil"
 	"github.com/garethgeorge/backrest/internal/orchestrator/logging"
 	"github.com/garethgeorge/backrest/internal/orchestrator/tasks"
+	"github.com/google/shlex"
 )
 
 type commandHandler struct{}
@@ -25,24 +28,33 @@ func (commandHandler) Execute(ctx context.Context, h *v1.Hook, vars interface{},
 	writer := logging.WriterFromContext(ctx)
 
 	// Parse out the shell to use if a #! prefix is present
-	shell := "sh"
+	shell := []string{"sh"}
+	if runtime.GOOS == "windows" {
+		shell = []string{"powershell", "-nologo", "-noprofile"}
+	}
+
 	if len(command) > 2 && command[0:2] == "#!" {
 		nextLine := strings.Index(command, "\n")
 		if nextLine == -1 {
 			nextLine = len(command)
 		}
-		shell = strings.Trim(command[2:nextLine], " ")
+		shell, err = shlex.Split(strings.Trim(command[2:nextLine], " "))
+		if err != nil {
+			return fmt.Errorf("parsing shell for command: %w", err)
+		} else if len(shell) == 0 {
+			return errors.New("must specify shell for command")
+		}
 		command = command[nextLine+1:]
 	}
 
 	scriptWriter := &ioutil.LinePrefixer{W: writer, Prefix: []byte("[script] ")}
-	defer scriptWriter.Close()
+	fmt.Fprintf(scriptWriter, "%v\n%v\n", shell, command)
+	scriptWriter.Close()
 	outputWriter := &ioutil.LinePrefixer{W: writer, Prefix: []byte("[output] ")}
 	defer outputWriter.Close()
-	fmt.Fprintf(scriptWriter, "------- script -------\n#! %v\n%v\n", shell, command)
 
 	// Run the command in the specified shell
-	execCmd := exec.Command(shell)
+	execCmd := exec.Command(shell[0], shell[1:]...)
 	execCmd.Stdin = strings.NewReader(command)
 
 	stdout := &ioutil.SynchronizedWriter{W: outputWriter}
