0ac5e2c5e29a31063b66c75d85f02574acb442de
garethgeorge <garethgeorge97@gmail.com>
2024-09-03 20:39:04 -0700

    finalize deprecation

--- internal/config/migrations/003relativescheduling.go
+++ internal/config/migrations/003relativescheduling.go
@@ -2,9 +2,11 @@ package migrations
 
 import (
 	v1 "github.com/garethgeorge/backrest/gen/go/v1"
+	"go.uber.org/zap"
 )
 
-func migration003RelativeScheduling(config *v1.Config) {
+var migration003RelativeScheduling = func(config *v1.Config) {
+	zap.L().Info("applying config migration 003: relative scheduling")
 	// loop over plans and examine prune policy's
 	for _, repo := range config.Repos {
 		prunePolicy := repo.GetPrunePolicy()
--- internal/config/migrations/migrations.go
+++ internal/config/migrations/migrations.go
@@ -4,32 +4,42 @@ import (
 	"fmt"
 
 	v1 "github.com/garethgeorge/backrest/gen/go/v1"
-	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
 )
 
-const migrationOffset = 2
-
-var migrations = []func(*v1.Config){
-	migration003RelativeScheduling, // version 3
+var migrations = []*func(*v1.Config){
+	&noop, // migration001PrunePolicy
+	&noop, // migration002Schedules is deprecated
+	&migration003RelativeScheduling,
 }
 
-var CurrentVersion = migrationOffset + int32(len(migrations))
+var CurrentVersion = int32(len(migrations))
 
 func ApplyMigrations(config *v1.Config) error {
-	curVersion := int(config.Version - 1)
-	if curVersion < 0 {
-		curVersion = 0
-	} else {
-		if curVersion < migrationOffset {
-			return fmt.Errorf("config version %d is too old to migrate. Please update to 1.4.0 first which is the newest version that _may_ be able to migrate from your version", config.Version)
+	if config.Version == 0 {
+		if proto.Equal(config, &v1.Config{}) {
+			config.Version = CurrentVersion
+			return nil
 		}
-		curVersion -= migrationOffset
+		return fmt.Errorf("config version 0 is invalid")
 	}
 
-	for idx := curVersion; idx < len(migrations); idx += 1 {
-		zap.S().Infof("applying config migration %d", idx+1)
-		migrations[idx](config)
+	startMigration := int(config.Version)
+	if startMigration < 0 {
+		startMigration = 0
+	}
+
+	for idx := startMigration; idx < len(migrations); idx += 1 {
+		m := migrations[idx]
+		if m == &noop {
+			return fmt.Errorf("config version %d is too old to migrate, please try first upgrading to backrest 1.4.0 which is the last version that may be compatible with your config", config.Version)
+		}
+		(*m)(config)
 	}
 	config.Version = CurrentVersion
 	return nil
+}
+
+var noop = func(config *v1.Config) {
+	// do nothing
 }
