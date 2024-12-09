fbec6459c7e3111c2a988759dbc485d3b62d0a1b
herobrauni <braunnicolaj@gmail.com>
2024-12-09 18:54:17 +0100

    change payload to text

--- internal/hook/types/healthchecks.go
+++ internal/hook/types/healthchecks.go
@@ -1,75 +1,64 @@
 package types
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"fmt"
-	"reflect"
+    "bytes"
+    "context"
+    "fmt"
+    "reflect"
 
-	v1 "github.com/garethgeorge/backrest/gen/go/v1"
-	"github.com/garethgeorge/backrest/internal/hook/hookutil"
-	"github.com/garethgeorge/backrest/internal/orchestrator/tasks"
-	"github.com/garethgeorge/backrest/internal/protoutil"
-	"go.uber.org/zap"
+    v1 "github.com/garethgeorge/backrest/gen/go/v1"
+    "github.com/garethgeorge/backrest/internal/hook/hookutil"
+    "github.com/garethgeorge/backrest/internal/orchestrator/tasks"
+    "github.com/garethgeorge/backrest/internal/protoutil"
+    "go.uber.org/zap"
 )
 
 type healthchecksHandler struct{}
 
 func (healthchecksHandler) Name() string {
-	return "healthchecks"
+    return "healthchecks"
 }
 
 func (healthchecksHandler) Execute(ctx context.Context, cmd *v1.Hook, vars interface{}, runner tasks.TaskRunner, event v1.Hook_Condition) error {
-	payload, err := hookutil.RenderTemplateOrDefault(cmd.GetActionHealthchecks().GetTemplate(), hookutil.DefaultTemplate, vars)
-	if err != nil {
-		return fmt.Errorf("template rendering: %w", err)
-	}
+    payload, err := hookutil.RenderTemplateOrDefault(cmd.GetActionHealthchecks().GetTemplate(), hookutil.DefaultTemplate, vars)
+    if err != nil {
+        return fmt.Errorf("template rendering: %w", err)
+    }
 
-	l := runner.Logger(ctx)
-	l.Sugar().Infof("Sending healthchecks message to %s", cmd.GetActionHealthchecks().GetWebhookUrl())
-	l.Debug("Sending healthchecks message", zap.String("payload", payload))
+    l := runner.Logger(ctx)
+    l.Sugar().Infof("Sending healthchecks message to %s", cmd.GetActionHealthchecks().GetWebhookUrl())
+    l.Debug("Sending healthchecks message", zap.String("payload", payload))
 
-	PingUrl := cmd.GetActionHealthchecks().GetWebhookUrl()
+    PingUrl := cmd.GetActionHealthchecks().GetWebhookUrl()
 
 	// Send a "start" signal to healthchecks.io when the hook is starting.
-	if protoutil.IsStartCondition(event) {
-		PingUrl += "/start"
-	}
+    if protoutil.IsStartCondition(event) {
+        PingUrl += "/start"
+    }
 
 	// Send a "fail" signal to healthchecks.io when the hook is failing.
-	if protoutil.IsErrorCondition(event) {
-		PingUrl += "/fail"
-	}
+    if protoutil.IsErrorCondition(event) {
+        PingUrl += "/fail"
+    }
 
 	// Send a "log" signal to healthchecks.io when the hook is ending.
 	if protoutil.IsLogCondition(event) {
-		PingUrl += "/log"
-	}
+        PingUrl += "/log"
+    }
 
-	type Message struct {
-		Text string `json:"text"`
-	}
+    body, err := hookutil.PostRequest(PingUrl, "text/plain", bytes.NewBufferString(payload))
+    if err != nil {
+        return fmt.Errorf("sending healthchecks message to %q: %w", PingUrl, err)
+    }
 
-	request := Message{
-		Text: payload,
-	}
-
-	requestBytes, _ := json.Marshal(request)
-
-	body, err := hookutil.PostRequest(PingUrl, "application/json", bytes.NewReader(requestBytes))
-	if err != nil {
-		return fmt.Errorf("sending healthchecks message to %q: %w", PingUrl, err)
-	}
-
-	l.Debug("Healthchecks response", zap.String("body", body))
-	return nil
+    l.Debug("Healthchecks response", zap.String("body", body))
+    return nil
 }
 
 func (healthchecksHandler) ActionType() reflect.Type {
-	return reflect.TypeOf(&v1.Hook_ActionHealthchecks{})
+    return reflect.TypeOf(&v1.Hook_ActionHealthchecks{})
 }
 
 func init() {
-	DefaultRegistry().RegisterHandler(&healthchecksHandler{})
+    DefaultRegistry().RegisterHandler(&healthchecksHandler{})
 }
