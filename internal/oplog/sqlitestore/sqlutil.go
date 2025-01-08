e3620f673296762d424bc74f7b85d391d4c32aa8
Gareth George <garethgeorge97@gmail.com>
2025-01-07 22:31:40 -0800

    chore: misc prerelease fixes

--- internal/oplog/sqlitestore/sqlutil.go
+++ internal/oplog/sqlitestore/sqlutil.go
@@ -5,9 +5,32 @@ import (
 	"zombiezen.com/go/sqlite/sqlitex"
 )
 
+// withSqliteTransaction should be used when the function only executes reads
 func withSqliteTransaction(conn *sqlite.Conn, f func() error) error {
 	var err error
 	endFunc := sqlitex.Transaction(conn)
+	err = f()
+	endFunc(&err)
+	return err
+}
+
+func withImmediateSqliteTransaction(conn *sqlite.Conn, f func() error) error {
+	var err error
+	endFunc, err := sqlitex.ImmediateTransaction(conn)
+	if err != nil {
+		return err
+	}
+	err = f()
+	endFunc(&err)
+	return err
+}
+
+func withExclusiveSqliteTransaction(conn *sqlite.Conn, f func() error) error {
+	var err error
+	endFunc, err := sqlitex.ExclusiveTransaction(conn)
+	if err != nil {
+		return err
+	}
 	err = f()
 	endFunc(&err)
 	return err
