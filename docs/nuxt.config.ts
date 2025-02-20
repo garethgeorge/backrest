f25b3cae3017cc401cd304664f069902fb120287
Gareth <garethgeorge97@gmail.com>
2025-02-19 23:04:16 -0800

    chore: cleanup documentation (#674)

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,7 +2,10 @@ export default defineNuxtConfig({
   extends: ["@nuxt-themes/docus"],
   devtools: { enabled: true },
   ssr: true,
+
   app: {
     baseURL: "/backrest/",
   },
+
+  compatibilityDate: "2025-02-19",
 });
